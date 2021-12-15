b292815a7afa85511d11cf25826286d057a7f4c5
hanawtnb <hanawtnb@icloud.com>
2021-12-15 10:32:56 +0900

    タイピングゲームの処理を実装。

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -24,13 +24,130 @@
         </div>
       </div>
       <br />
-
-      <button v-if="clearFlg" @click="init">戻る</button>
-      <!-- <input type="text" v-model="keyWord" />
-      <button @click="getWords(keyWord)">search</button> -->
     </div>
   </div>
 </template>
+
+<script lang="ts">
+import {
+  defineComponent,
+  ref,
+  onMounted,
+  watch,
+  computed,
+  nextTick,
+} from "vue";
+
+export default defineComponent({
+  setup() {
+    //ゲームが開始しているかのフラグ
+    let startFlg = ref(false);
+
+    /**
+     * ゲーム開始.
+     */
+    const gameStart = () => {
+      //スタートしているかのフラグ
+      startFlg.value = true;
+      console.log(startFlg.value);
+      //startボタンを押すとinputにフォーカスする。
+      nextTick(() => {
+        document.getElementById("typeForm").focus();
+      });
+    };
+    //現在の問題
+    const currentQuestion = ref("");
+    //問題集
+    const questions = ref([
+      "apple",
+      "banana",
+      "chocolate",
+      "coffee",
+      "yay",
+      "hey",
+      "hoo",
+      "wooo",
+    ]);
+
+    /**
+     * 問題集の配列を順番に表示.
+     */
+    onMounted(() => {
+      currentQuestion.value = questions.value[0];
+      questionCount.value = questions.value.length;
+    });
+
+    //入力欄
+    const typeBox = ref("");
+
+    /**
+     * 答え合わせ.
+     *
+     * @remarks 入力された文字と問題を比較して正解を確認。
+     *
+     */
+    watch(typeBox, (input) => {
+      if (input == currentQuestion.value) {
+        //問題の配列から正解した問題を削除
+        questions.value.splice(0, 1);
+        currentQuestion.value = questions.value[0];
+        //入力欄を空にする
+        typeBox.value = "";
+        //正解数の表示を１プラス
+        currentQuestionCount.value = currentQuestionCount.value + 1;
+      }
+    });
+    //現在の問題番号
+    let currentQuestionCount = ref(0);
+    //全問題数
+    let questionCount = ref(0);
+
+    //クリアしたかのフラグ
+    const clearFlg = ref(false);
+    //ゲージの増える量を調整する。
+    const gaugeAdjuster = ref(0);
+    //ゲージが増える処理
+    const styleObject = computed(() => {
+      //変数width_valueに(100% / 問題数 + 減ってしまった分を足す変数i)代入
+      const widthValue = 100 / (questions.value.length + gaugeAdjuster.value);
+      const width = widthValue * currentQuestionCount.value + "%";
+      let color = "";
+      if (
+        currentQuestionCount.value ==
+        questions.value.length + gaugeAdjuster.value
+      ) {
+        //全問正解
+        color = "rgb(255, 123, 0";
+        //クリアしたら戻るボタンを表示するためのフラグ
+        // eslint-disable-next-line vue/no-side-effects-in-computed-properties
+        clearFlg.value = true;
+      } else {
+        //回答中
+        color = " #FFA07A";
+      }
+      // eslint-disable-next-line vue/no-side-effects-in-computed-properties
+      gaugeAdjuster.value++;
+      return {
+        width: width,
+        "background-color": color,
+      };
+    });
+
+    return {
+      startFlg,
+      gameStart,
+      currentQuestion,
+      questions,
+      onMounted,
+      typeBox,
+      currentQuestionCount,
+      questionCount,
+      styleObject,
+      clearFlg,
+    };
+  },
+});
+</script>
 
 <style scoped>
 * {
