d1225b15987bdcd689a1e77f9b07df92201672b0
hanawtnb <hanawtnb@icloud.com>
2021-12-15 11:08:10 +0900

    コードを修正。

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -84,18 +84,18 @@ export default defineComponent({
      */
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const arrayShuffle = (array: any) => {
-      for (var i = array.length - 1; 0 < i; i--) {
+      for (let i = array.length - 1; 0 < i; i--) {
         // 0〜(i+1)の範囲で値を取得
-        var r = Math.floor(Math.random() * (i + 1));
+        const r = Math.floor(Math.random() * (i + 1));
 
         // 要素の並び替えを実行
-        var tmp = array[i];
+        const tmp = array[i];
         array[i] = array[r];
         array[r] = tmp;
       }
       return array;
     };
-    for (var i = 0; i < 5; i++) {
+    for (let i = 0; i < 5; i++) {
       console.log(arrayShuffle(questions.value));
     }
     //入力欄
